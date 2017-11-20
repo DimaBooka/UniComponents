ac2e6468d9883fb84ff76bb67e8637d307a69497
Dima Booka <booka@Dimas-MacBook-Pro.local>
2017-11-20 16:57:22 +0200

    added redirect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { UsersService } from './shared/services/users.service';
 import { ToasterConfig } from 'angular2-toaster';
 import { toastConfig } from './shared/toast.config';
+import {Router} from "@angular/router";
+import {AuthService} from "./shared/services/auth.service";
 
 @Component({
   selector: 'app-root',
@@ -13,11 +15,27 @@ export class AppComponent {
   public toasterconfig : ToasterConfig = new ToasterConfig(toastConfig);
 
   constructor(
-    public usersService: UsersService
+    public usersService: UsersService,
+    private router: Router,
+    private authService: AuthService
   ) {
     this.sideBarIsShows = this.usersService.isLogged.getValue();
     this.usersService.isLogged.subscribe((isLoggedIn: boolean) => {
       this.sideBarIsShows = isLoggedIn;
     });
+
+    if (this.router.isActive('/', true)) {
+      if (this.authService.getToken()) {
+        this.usersService.getPermissions().subscribe(permissions => {
+          if (permissions === 'Admin') {
+            this.router.navigate(['/games']);
+          } else {
+            this.router.navigate(['/games/partners']);
+          }
+        });
+      } else {
+        this.router.navigate(['/login']);
+      }
+    }
   }
 }
--- src/app/users/list-users/list-users.component.ts
+++ src/app/users/list-users/list-users.component.ts
@@ -3,6 +3,8 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UsersService } from '../../shared/services/users.service';
 import { User } from '../../shared/models/user.model';
+import {Partner} from "../../shared/models/partner.model";
+import {PartnersService} from "../../shared/services/partners.service";
 
 @Component({
   selector: 'app-list-users',
@@ -11,21 +13,29 @@ import { User } from '../../shared/models/user.model';
 })
 export class ListUsersComponent implements OnInit {
 
-  public users: User[] = [];
+  public partners: Partner[] = [];
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private modalService: NgbModal,
-    private usersService: UsersService
+    private usersService: UsersService,
+    private partnersService: PartnersService
   ) { }
 
   ngOnInit() {
-    this.updateListUsers();
+    // this.updateListUsers();
+    this.updateListPartners();
   }
 
-  private updateListUsers() {
-    this.usersService.getUsersList().subscribe((users: User[]) => {
-      this.users = users;
+  // private updateListUsers() {
+  //   this.usersService.getUsersList().subscribe((users: User[]) => {
+  //     this.users = users;
+  //   });
+  // }
+
+  private updateListPartners() {
+    this.partnersService.getPartnersList().subscribe((partners: Partner[]) => {
+      this.partners = partners;
     });
   }
 }
